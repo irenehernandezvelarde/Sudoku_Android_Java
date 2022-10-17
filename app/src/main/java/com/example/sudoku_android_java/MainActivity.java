04f9ba9c0551f0de30ebf78d205ee0e0c2436a09
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-10-17 16:57:25 +0200

    //Add app title.//Add tablelayout. // Create table rows. //Create spinners. //Add spinner on rows. //Add rows on table.

--- app/src/main/java/com/example/sudoku_android_java/MainActivity.java
+++ app/src/main/java/com/example/sudoku_android_java/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sudoku_android_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TableLayout table = findViewById(R.id.table);
+
+        CharSequence[] nombres = {"0","1","2","3","4","5","6","7","8","9"};
+        for(int i = 0; i <= 9; i++){
+            TableRow row = new TableRow(this);
+            for(int j = 0; j <= 9; j++) {
+                Spinner spinner = new Spinner(this);
+                spinner.setBackground(null);
+                spinner.setPadding(5, 5, 5, 5);
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+                        android.R.layout.simple_spinner_item, nombres);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                row.addView(spinner);
+            }
+            table.addView(row);
+        }
+
     }
 }
