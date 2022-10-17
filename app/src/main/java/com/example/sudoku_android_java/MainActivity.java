7aaa5903c2751c3a5336b616359b451c09de9292
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-10-17 17:32:19 +0200

    // Change font title.

--- app/src/main/java/com/example/sudoku_android_java/MainActivity.java
+++ app/src/main/java/com/example/sudoku_android_java/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TableLayout table = findViewById(R.id.table);
 
-        CharSequence[] nombres = {"0","1","2","3","4","5","6","7","8","9"};
+        CharSequence[] nombres = {"X","1","2","3","4","5","6","7","8","9"};
         for(int i = 0; i <= 9; i++){
             TableRow row = new TableRow(this);
             for(int j = 0; j <= 9; j++) {
