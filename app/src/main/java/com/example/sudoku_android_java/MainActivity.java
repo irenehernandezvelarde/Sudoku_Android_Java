89be26f08801df9744d0106aa8fbeb5b8b942158
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-10-21 18:47:09 +0200

    // Add sudoku background color. // Add onItemSelected function. // Add onNothingSelected function.

--- app/src/main/java/com/example/sudoku_android_java/MainActivity.java
+++ app/src/main/java/com/example/sudoku_android_java/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sudoku_android_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
@@ -14,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         TableLayout table = findViewById(R.id.table);
 
         CharSequence[] nombres = {"X","1","2","3","4","5","6","7","8","9"};
@@ -21,8 +27,23 @@ public class MainActivity extends AppCompatActivity {
             TableRow row = new TableRow(this);
             for(int j = 0; j <= 9; j++) {
                 Spinner spinner = new Spinner(this);
+                spinner.setTag(R.id.fila,i);
+                spinner.setTag(R.id.col,j);
                 spinner.setBackground(null);
                 spinner.setPadding(5, 5, 5, 5);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        int fila = (int) adapterView.getTag(R.id.fila);
+                        int col = (int) adapterView.getTag(R.id.col);
+                        String string = spinner.getSelectedItem().toString();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                         android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,6 +52,6 @@ public class MainActivity extends AppCompatActivity {
             }
             table.addView(row);
         }
-
     }
+
 }
