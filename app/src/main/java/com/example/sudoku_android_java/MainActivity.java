f9123ec4ce5e4160317a81b17ede24391a70e019
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-11-07 18:19:22 +0100

    //Crear numeros aleatorios.

--- app/src/main/java/com/example/sudoku_android_java/MainActivity.java
+++ app/src/main/java/com/example/sudoku_android_java/MainActivity.java
@@ -13,54 +13,89 @@ import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Model model=new Model();
+    private Spinner[][] matriu=new Spinner[9][9];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TableLayout tablita= findViewById(R.id.table);
+        CharSequence[] nombres = {"*","1","2","3","4","5","6","7","8","9"};
 
 
+        for(int i=0;i<9;i++){
+            TableRow rowy=new TableRow(this);
+            for(int j=0;j<9;j++){
 
-        TableLayout table = findViewById(R.id.table);
-
-        CharSequence[] nombres = {"X","1","2","3","4","5","6","7","8","9"};
-        for(int i = 0; i <= 9; i++){
-            TableRow row = new TableRow(this);
-            for(int j = 0; j <= 9; j++) {
-                Spinner spinner = new Spinner(this);
-                spinner.setTag(R.id.fila,i);
-                spinner.setTag(R.id.col,j);
-                spinner.setTag("bug init");
-                spinner.setBackground(null);
-                spinner.setPadding(5, 5, 5, 5);
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                Spinner sp=new Spinner(this);
+                sp.setBackground(null);
+                sp.setPadding(5, 5, 5, 5);
+                sp.setTag(R.id.col,j);
+                sp.setTag(R.id.fila,i);
+                sp.setTag("bug init");
+                sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, int k, long l) {
+                        // la posició del spinner és 'i', però també es pot buscar amb
                         int fila = (int) adapterView.getTag(R.id.fila);
                         int col = (int) adapterView.getTag(R.id.col);
-                        if( spinner.getTag().equals("bug init")){
-                            spinner.setTag("hecho");
+                        if (sp.getTag().equals("bug init")){
+                            sp.setTag("guay");
                             return;
                         }
-                        String text = spinner.getSelectedItem().toString();
-                        Toast.makeText(MainActivity.this, "fila: "+ fila +" columna: " + col + " nuevo valor: "+text, Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(MainActivity.this, "fila: "+fila+" columna: "+col+" nuevo valor: "+k, Toast.LENGTH_SHORT).show();
+                        //sp..setSelection(getIndex(spinner, myString));
+                        //spinner.getItemAtPosition(i)
+                        if(model.setVal(k,fila,col)>-1){
+                            refrescaGUI();
+                        }
+                        else{
+                            matriu[fila][col].setSelection(0);
+                        }
 
                     }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> adapterView) {
-
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // Another interface callback
                     }
+
                 });
+
+
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                         android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
-                row.addView(spinner);
+                sp.setAdapter(adapter);
+                this.matriu[i][j]=sp;
+                rowy.addView(this.matriu[i][j]);
+
+
             }
-            table.addView(row);
+            tablita.addView(rowy);
+
+        }
+        refrescaGUI();
+
+    }
+
+    private void refrescaGUI(){
+        for(int i=0;i<9;i++) {
+
+            for (int j = 0; j < 9; j++) {
+                matriu[i][j].setSelection(model.getVal(i,j));
+            }
         }
     }
 
--- app/src/main/java/com/example/sudoku_android_java/Model.java
+++ app/src/main/java/com/example/sudoku_android_java/Model.java
@@ -1,72 +1,109 @@
 package com.example.sudoku_android_java;
 
+import android.util.Log;
+
 import java.util.Arrays;
+import java.util.Random;
 
 public class Model {
+    private static int[][] matriuI=new int[9][9];
 
-    int matriz[][] = new int[9][9];
-
-    private int getVal(int fila, int columna) {
-    return matriz[fila][columna];
+    public int getVal(int fila,int columna){
+        return matriuI[fila][columna];
+    };
+    public int setVal(int valor,int fila,int columna){
+        int previousVal = matriuI[fila][columna];
+        matriuI[fila][columna] = valor;
+        if (!comprovaFila(fila)){matriuI[fila][columna] = previousVal; return -1;};
+        if (!comprovaCol(columna)){matriuI[fila][columna] = previousVal; return -1;};
+        if (!comprovaQuad(fila,columna)){matriuI[fila][columna] = previousVal; return -1;};
+        return 0;
     }
-    private int setVal(int valor, int fila, int columna) {
-        if(valor < 1 || valor > 9 ) {
-            this.matriz[fila][columna] = valor;
-            return matriz[fila][columna];
-        }
-        return matriz[fila][columna];
-    }
-
-    private boolean comprovaFila(int fila) {
-        for(int i=0; i < matriz.length; i++){
-            int array []= new int[0];
-            if((Arrays.asList(array).contains(matriz[fila][i]))){
-                return false;
-            }else{
-                array[i]=(matriz[fila][i]);
-            }
-            return true;
-        }
-    return true;
-    }
-
-    private boolean comprovaCol(int columna) {
-        for(int i=0; i < matriz.length; i++){
-            int array []= new int[0];
-            if((Arrays.asList(array).contains(matriz[i][columna]))){
-                return false;
-            }else{
-                array[i]=(matriz[i][columna]);
-            }
-            return true;
-        }
-        return true;
-    }
-
-    private boolean comprovaQuad (int fila, int columna) {
-    return true;
-    }
-
-    private void creaPartida () {
-        boolean bucle = true;
-        int contador = 0;
-        while(bucle){
-            int i = (int) (Math.random()*9);
-            int j = (int) (Math.random()*9);
-            int v = (int) (Math.random()*9);
-            if(getVal(i,j) == 0 && v>0){
-                setVal(v,i,j);
-                if(comprovaFila(i) && comprovaCol(j)){
-                    contador++;
-
-                } else{
-                    setVal(0,i,j);
+    public boolean comprovaFila(int fila){
+        Boolean valid = true;
+        for (int a = 0; a<9; a++){
+            for (int b = 0; b<9; b++){
+                if (a != b && matriuI[fila][a] != 0){
+                    if (matriuI[fila][a] == matriuI[fila][b]){return false; }
                 }
             }
-            if(contador == 20){
-                bucle = false;
+        }
+
+        return valid;
+    }
+    public boolean comprovaCol(int columna){
+        boolean valid = true;
+        for (int a = 0; a < 9; a++){
+            for (int b = 0; b < 9; b++){
+                if (a != b && matriuI[b][columna] != 0){
+                    if (matriuI[a][columna] == matriuI[b][columna]){return false;}
+                }
+            }
+        }
+
+        return valid;
+    }
+    public boolean comprovaQuad(int columna,int fila){
+        boolean valid = true;
+        int minfila = 0;
+        int mincolumna = 0;
+        //Determina la cantonada superior esquerra del quadrant
+        if (fila < 3){
+            minfila = 0;
+            if (columna<3) {mincolumna = 0;}
+            else if (columna < 6){mincolumna = 3;}
+            else {mincolumna = 6;}
+        }else if (fila < 6) {
+            minfila = 3;
+            if (columna<3) {mincolumna = 0;}
+            else if (columna < 6){mincolumna = 3;}
+            else {mincolumna = 6;}
+        } else {
+            minfila = 6;
+            if (columna<3) {mincolumna = 0;}
+            else if (columna < 6){mincolumna = 3;}
+            else {mincolumna = 6;}
+        }
+        //Comprova els valors del quadrant
+        for (int a = minfila; a < minfila+3 ; a++){
+            for (int b = mincolumna; b < mincolumna + 3; b++) {
+                for (int a1 = minfila; a1 < minfila + 3; a1++) {
+                    for (int b1 = mincolumna; b1 < mincolumna + 3; b1++) {
+                        if (a != a1 && b != b1 && matriuI[a][b] != 0) {
+                            if (matriuI[a][b] == matriuI[a1][b1]) {
+                                Log.i("Quad", "NO");
+                                valid = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Log.i("Quad", "YES");
+        return valid;
+    }
+
+    public void creaPartida(){
+        Random rng = new Random();
+        int numsIntroduits = 20;
+        Log.i("Inputs",String.valueOf(numsIntroduits));
+        int row = 0;
+        int col = 0;
+        int val = 0;
+        for (int a = 0; a < numsIntroduits; a++){
+            row = rng.nextInt(9-1);
+            col = rng.nextInt(9-1);
+            val = rng.nextInt(9-1)+1;
+            setVal(row,col,val);
+            while (setVal(row,col,val) == -1){
+                Log.i("INFO","Regenerant nombre");
+                row = rng.nextInt(9-1);
+                col = rng.nextInt(9-1);
             }
         }
     }
 
+    public Model() {
+        creaPartida();
+    }
 }
